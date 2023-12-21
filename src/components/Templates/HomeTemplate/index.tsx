114567f8c7ace4256d47491e013cc8d742ec23dd
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2023-12-21 15:12:18 -0300

    criacao da sectio about

--- src/components/Templates/HomeTemplate/index.tsx
+++ src/components/Templates/HomeTemplate/index.tsx
@@ -8,31 +8,121 @@ export const HomeTemplate = () => {
     <LayoutTemplate>
       <S.Container>
         <S.SectioStarCheck>
-          <S.SectioStarCheckLeftSide>
-            <S.SectioStarCheckLeftSideWrapper>
-              <h1>
-                Star{" "}
-                <span>
-                  Check{" "}
-                  <img src="assets/imgs/check-icon.svg" alt="icone check" />
-                </span>
-              </h1>
+          <S.SectioStarCheckContent>
+            <S.SectioStarCheckLeftSide>
+              <S.SectioStarCheckLeftSideWrapper>
+                <S.Title>
+                  Star{" "}
+                  <span>
+                    Check{" "}
+                    <img src="assets/imgs/check-icon.svg" alt="icone check" />
+                  </span>
+                </S.Title>
 
-              <p>
-                Bem-vindo à StarCheck, sua empresa de Vistoria Veicular,
-                especializada em fornecer análises precisas e fornecidas do seu
-                veículo, garantindo segurança e complementos.
-                <span>Vistoria Veicularh</span>
-              </p>
-              <ButtonHome>AGENDAR VISTORIA</ButtonHome>
-            </S.SectioStarCheckLeftSideWrapper>
-          </S.SectioStarCheckLeftSide>
-          <S.SectioStarCheckRightSide>
-            <S.SectioStarCheckRightSideWrapper>
-              <img src="assets/imgs/Rectangle.png" alt="foto mecanico" />
-            </S.SectioStarCheckRightSideWrapper>
-          </S.SectioStarCheckRightSide>
+                <p>
+                  Bem-vindo à StarCheck, sua empresa de Vistoria Veicular,
+                  especializada em fornecer análises precisas e fornecidas do
+                  seu veículo, garantindo segurança e complementos.
+                  <span>Vistoria Veicularh</span>
+                </p>
+                <ButtonHome>AGENDAR VISTORIA</ButtonHome>
+              </S.SectioStarCheckLeftSideWrapper>
+            </S.SectioStarCheckLeftSide>
+            <S.SectioStarCheckRightSide>
+              <S.SectioStarCheckRightSideWrapper>
+                <img src="assets/imgs/Rectangle.png" alt="foto mecanico" />
+              </S.SectioStarCheckRightSideWrapper>
+            </S.SectioStarCheckRightSide>
+          </S.SectioStarCheckContent>
         </S.SectioStarCheck>
+        <S.SectionAbout>
+          <S.SectionAboutContent>
+            <S.Title>
+              Sobre
+              <span>
+                Nós <img src="assets/imgs/check-icon.svg" alt="icone check" />
+              </span>
+            </S.Title>
+            <S.TextDefault>
+              A <S.TextStrong>StarCheck</S.TextStrong> destaca-se pela sua{" "}
+              <S.TextStrong>precisão</S.TextStrong>,{" "}
+              <S.TextStrong>compromisso</S.TextStrong> com a{" "}
+              <S.TextStrong>qualidade</S.TextStrong> e{" "}
+              <S.TextStrong>atendimento excepcional</S.TextStrong> ao cliente.
+              Valorizamos sua satisfação e nos esforçamos para superar suas
+              expectativas em cada interação. Nossa equipe está pronta para
+              responder às suas perguntas, fornecer orientações especializadas e
+              garantir que suas necessidades sejam atendidas de maneira
+              eficiente.
+            </S.TextDefault>
+            <S.TextDefault>
+              Seja para uma{" "}
+              <S.TextBlue>
+                vistoria em sua residência, endereço comercial (móvel)
+              </S.TextBlue>{" "}
+              ou <S.TextBlue>direto em nossos postos (fixos)</S.TextBlue>, a{" "}
+              <S.TextStrong>StarCheck</S.TextStrong> está pronta para ser sua
+              parceira confiável em todas as suas necessidades de vistorias.
+              Conte conosco para, promover a segurança e proporcionar
+              tranquilidade.
+            </S.TextDefault>
+            <S.TextDefault>
+              <S.TextStrong>Agende uma vistoria</S.TextStrong> conosco hoje
+              mesmo e experimente o profissionalismo e a excelência que trouxe a{" "}
+              <S.TextStrong>StarCheck </S.TextStrong>a melhor do Maranhão.
+              Estamos ansiosos para trabalhar com você e oferecer soluções em
+              vistorias.
+            </S.TextDefault>
+
+            <ButtonHome>AGENDAR VISTORIA</ButtonHome>
+
+            <S.SectionCards>
+              <S.WrapperCard>
+                <h3>Missão</h3>
+                <S.Card>
+                  <p>
+                    Contribuir para a segurança no mercado de{" "}
+                    <S.TextBlue>veículos usados </S.TextBlue>
+                    ​​e <S.TextBlue>seminovos</S.TextBlue>, realizando revisões
+                    confiáveis ​​e precisas que garantem a tranquilidade de
+                    nossos clientes.
+                  </p>
+                </S.Card>
+              </S.WrapperCard>
+
+              <S.WrapperCard>
+                <h3>Visão</h3>
+                <S.Card>
+                  <p>
+                    Nosso propósito é ser a principal referência em{" "}
+                    <S.TextBlue>Vistoria Veicular</S.TextBlue>, destacando-nos
+                    pela excelência no serviço prestado e pela dedicação em
+                    atender às necessidades de nossos clientes.
+                  </p>
+                </S.Card>
+              </S.WrapperCard>
+
+              <S.WrapperCard>
+                <h3>Valores</h3>
+                <S.Card>
+                  <S.Grid gridTemplate=".2fr 1fr" gap="16px">
+                    <img src="assets/imgs/check-icon.svg" alt="" />
+                    <h4>INTEGRIDADE</h4>
+
+                    <img src="assets/imgs/check-icon.svg" alt="" />
+                    <h4>COMPROMETIMENTO</h4>
+
+                    <img src="assets/imgs/check-icon.svg" alt="" />
+                    <h4>RESPEITO</h4>
+
+                    <img src="assets/imgs/check-icon.svg" alt="" />
+                    <h4>CONFIANÇA</h4>
+                  </S.Grid>
+                </S.Card>
+              </S.WrapperCard>
+            </S.SectionCards>
+          </S.SectionAboutContent>
+        </S.SectionAbout>
       </S.Container>
     </LayoutTemplate>
   );
--- src/components/Templates/HomeTemplate/styles.ts
+++ src/components/Templates/HomeTemplate/styles.ts
@@ -1,19 +1,23 @@
 import styled, { css } from "styled-components";
 
 export const Container = styled.div`
-  ${({ theme: { space } }) => css`
-    background: #eee;
-    height: 720px;
-    padding-top: 90px;
-  `}
+  ${({ theme: { space } }) => css``}
 `;
 
-export const SectioStarCheck = styled.div`
+export const SectioStarCheckContent = styled.div`
   ${({ theme: { space } }) => css`
     max-width: ${space.medio};
     margin: 0 auto;
     display: flex;
     height: 100%;
+  `}
+`;
+
+export const SectioStarCheck = styled.div`
+  ${({ theme: { space } }) => css`
+    background: #eee;
+    height: 720px;
+    padding-top: 90px;
   `}
 `;
 
@@ -25,23 +29,23 @@ export const SectioStarCheckLeftSide = styled.div`
   `}
 `;
 
+export const Title = styled.h2`
+  color: #111;
+  font-family: "Poppins";
+  font-size: 72px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 80px;
+  margin-bottom: 24px;
+
+  span {
+    display: block;
+  }
+`;
+
 export const SectioStarCheckLeftSideWrapper = styled.div`
   ${({ theme: { space } }) => css`
     max-width: 80%;
-
-    h1 {
-      color: #111;
-      font-family: "Poppins";
-      font-size: 72px;
-      font-style: normal;
-      font-weight: 700;
-      line-height: 80px;
-      margin-bottom: 24px;
-
-      span {
-        display: block;
-      }
-    }
 
     p {
       color: #2d2d2d;
@@ -100,4 +104,109 @@ export const SectioStarCheckRightSideWrapper = styled.div`
       height: 314px;
     }
   `}
+`;
+
+export const SectionAbout = styled.div`
+  padding: 120px 0;
+`;
+
+export const SectionAboutContent = styled.div`
+  ${({ theme: { space } }) => css`
+    max-width: ${space.medio};
+    margin: 0 auto;
+
+    button {
+      margin: 0 auto;
+      display: block;
+      margin: 70px auto;
+    }
+  `}
+`;
+
+export const TextBlue = styled.span`
+  color: #266bf0;
+`;
+
+export const TextDefault = styled.p`
+  margin-bottom: 24px;
+  color: #2d2d2d;
+  font-family: Roboto;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px; /* 160% */
+`;
+
+export const TextStrong = styled.span`
+  font-weight: 700;
+`;
+
+export const SectionCards = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const WrapperCard = styled.div`
+  width: max-content;
+
+  h3 {
+    color: #111;
+    text-align: center;
+    font-family: "Poppins";
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 32px;
+    margin-bottom: 40px;
+  }
+
+  h4 {
+    color: #eee;
+    font-family: "Roboto";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 32px;
+  }
+`;
+
+export const Grid = styled.div<{ gridTemplate: string; gap: string }>`
+  ${({ gridTemplate, gap }) => css`
+    display: grid;
+    grid-template-columns: ${gridTemplate};
+    gap: ${gap};
+
+    img {
+      display: block;
+      width: 20px;
+      margin-top: 6px;
+    }
+  `}
+`;
+
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 360px;
+  height: 360px;
+  padding: 0 18px;
+  border-radius: 5px;
+  background: linear-gradient(
+    217deg,
+    #00186d -10.67%,
+    #002ed1 -10.66%,
+    #00114d 78.45%
+  );
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  p {
+    color: #eee;
+    text-align: center;
+    font-family: "Roboto";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 32px;
+  }
 `;
