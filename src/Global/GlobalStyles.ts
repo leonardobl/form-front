b58723e6dab8cb22812f50d7ed2e9a692cac65cd
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2023-12-21 10:28:29 -0300

    primeiro commit

---
+++ src/Global/GlobalStyles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+}
+
+
+
+html, body, #root {
+  min-width: 100vh;
+}
+
+
+
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import { GlobalStyles } from "./Global/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { Theme } from "./Global/Theme";
+import { Home } from "./components/Pages/home";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
