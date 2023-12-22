2a36fb87b1b40069695568def051866b1e9e9229
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2023-12-22 14:57:19 -0300

    criação do tema para o MUI

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,10 @@ import { ThemeProvider } from "styled-components";
 import { Theme } from "./Global/Theme";
 import { Home } from "./components/Pages/home";
 import { Scheduling } from "./components/Pages/scheduling";
-import { Route, Routes, BrowserRouter, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { ThemeProvider as MUIThemeProvider } from "@mui/material";
 import { Login } from "./components/Pages/login";
+import { MUI_Theme } from "./Global/MUI-Theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,15 +17,16 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
-      <GlobalStyles />
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="agendamento" element={<Scheduling />} />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <MUIThemeProvider theme={MUI_Theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="agendamento" element={<Scheduling />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </MUIThemeProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
