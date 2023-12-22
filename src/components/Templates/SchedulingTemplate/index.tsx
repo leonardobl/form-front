35543e763cd0e46152a78ff3127b66cbba9674e3
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2023-12-22 09:43:23 -0300

    criação da pagina de agendamento

--- src/components/Templates/SchedulingTemplate/index.tsx
+++ src/components/Templates/SchedulingTemplate/index.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { LayoutTemplate } from "../LayoutTemplate";
+import * as S from "./styles";
+import { ButtonHome } from "../../Atoms/ButtonHome";
 
 export const SchedulingTemplate = () => {
   return (
     <LayoutTemplate>
-      <div>SchedulingTemplate</div>;
+      <S.Container>
+        <S.Banner>
+          <S.BannerLeftSide>
+            <S.BannerLeftSideContent>
+              <h1>
+                Agende sua <br /> vistoria veicular
+              </h1>
+
+              <p>
+                Agende sua vistoria de <S.TitleBlue>forma rápida</S.TitleBlue> e
+                <S.TitleBlue>fácil.</S.TitleBlue> Não perca tempo e garanta sua
+                segurança!
+              </p>
+            </S.BannerLeftSideContent>
+          </S.BannerLeftSide>
+          <S.BannerRightSide></S.BannerRightSide>
+        </S.Banner>
+        <S.Preference>
+          <p>
+            Escolha onde você deseja executar sua vistoria veicular, na{" "}
+            <S.TitleBlue>loja física</S.TitleBlue> ou em{" "}
+            <S.TitleBlue>endereço de sua preferência.</S.TitleBlue>
+          </p>
+
+          <S.WrapperButtons>
+            <ButtonHome>Loja Física</ButtonHome>
+            <ButtonHome>Domicilio</ButtonHome>
+          </S.WrapperButtons>
+        </S.Preference>
+      </S.Container>
     </LayoutTemplate>
   );
 };
---
+++ src/components/Templates/SchedulingTemplate/styles.ts
@@ -0,0 +1,77 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  ${({ theme: { space } }) => css`
+    max-width: ${space.large};
+  `}
+`;
+
+export const Banner = styled.div`
+  height: 720px;
+  display: flex;
+  background-image: url("assets/imgs/banner-agendamento.svg");
+  background-size: cover;
+`;
+
+export const BannerLeftSide = styled.div`
+  flex: 1;
+
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+
+export const BannerLeftSideContent = styled.div`
+  max-width: 620px;
+  h1 {
+    color: #eee;
+    font-family: "Poppins";
+    font-size: 72px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 80px;
+    margin-bottom: 40px;
+  }
+
+  p {
+    color: #eee;
+    font-family: "Roboto";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 32px;
+  }
+`;
+
+export const BannerRightSide = styled.div`
+  flex: 1;
+`;
+
+export const TitleBlue = styled.span`
+  color: #266bf0;
+`;
+
+export const Preference = styled.div`
+  padding: 180px 0;
+
+  p {
+    color: #2d2d2d;
+    text-align: center;
+    font-family: "Roboto";
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 32px;
+    max-width: 750px;
+    display: block;
+    margin: auto;
+    margin-bottom: 80px;
+  }
+`;
+
+export const WrapperButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 72px;
+  align-items: center;
+`;
