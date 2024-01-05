e5b41cb58eb9d9c8051b035fc83eeb5c2a9bd9bd
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2024-01-05 10:07:51 -0300

    criacao dos services de pagamento

--- src/services/Pagamento/index.ts
+++ src/services/Pagamento/index.ts
@@ -2,11 +2,15 @@ import { AxiosResponse } from "axios";
 import { ApiBrave } from "../../Apis/Brave/index";
 import { removeEmpty } from "../../utils/removeEmpty";
 import objectToParams from "../../utils/objectToParams";
-import { IFaturaDTO } from "../../types/pagamento.s";
+import {
+  IFaturaDTO,
+  IFaturaResponse,
+  IFormStatusPagamento,
+} from "../../types/pagamento";
 
 const basePath = "/pagamento";
 
-export class Municipio {
+export class Pagamento {
   static async get({
     uuidAgendamento,
   }: {
@@ -14,6 +18,20 @@ export class Municipio {
   }): Promise<AxiosResponse<IFaturaDTO>> {
     return ApiBrave.get(
       `${basePath}/agendamento/${uuidAgendamento}/consultar-fatura`
+    );
+  }
+
+  static async getStatus({ form }: { form: IFormStatusPagamento }) {
+    return ApiBrave.get(`${basePath}/status-pagamento`);
+  }
+
+  static async post({
+    uuidAgendamento,
+  }: {
+    uuidAgendamento: string;
+  }): Promise<AxiosResponse<IFaturaResponse>> {
+    return ApiBrave.get(
+      `${basePath}/agendamento/${uuidAgendamento}/gerar-fatura`
     );
   }
 }
