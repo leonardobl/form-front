4359ac45994b1ec4bf30589607df525e9b4e7a1f
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2024-01-10 17:01:21 -0300

    ajuste input cpf login

--- src/components/Templates/LoginTemplate/index.tsx
+++ src/components/Templates/LoginTemplate/index.tsx
@@ -19,10 +19,10 @@ export const LoginTemplate = () => {
   const { isLoad, setIsLoad } = useContextSite();
   const [isDisable, setIsDisable] = useState(false);
 
-  function handleCpf(e: string) {
-    const newCpfValue = maskCpf(e);
-    setForm((prev) => ({ ...prev, cpfCNPJ: newCpfValue }));
-  }
+  // function handleCpf(e: string) {
+  //   const newCpfValue = maskCpf(e);
+  //   setForm((prev) => ({ ...prev, cpfCNPJ: newCpfValue }));
+  // }
 
   function login(e: React.SyntheticEvent) {
     e.preventDefault();
@@ -68,8 +68,12 @@ export const LoginTemplate = () => {
               <label>CPF/CNPJ</label>
               <InputCustom
                 value={form?.cpfCNPJ}
-                maxLength={14}
-                onChange={(e) => handleCpf(e.target.value)}
+                type="number"
+                // maxLength={14}
+                onChange={(e) => {
+                  setForm((prev) => ({ ...prev, cpfCNPJ: e.target.value }));
+                  // handleCpf(e.target.value);
+                }}
                 required
                 // value={form.email}
                 // onChange={(e) =>
