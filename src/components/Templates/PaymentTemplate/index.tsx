7f5cf8f2576f8bca45eca2a120a40443961bec87
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2024-01-30 15:29:19 -0300

    remocaçao gerar fatura

--- src/components/Templates/PaymentTemplate/index.tsx
+++ src/components/Templates/PaymentTemplate/index.tsx
@@ -33,18 +33,18 @@ export const PaymentTemplate = () => {
     if (!agendamento) return;
     setIsLoad(true);
 
-    Pagamento.gerarFatura({ uuidAgendamento: agendamento?.uuid })
-      .then(({ data }) => {
-        setPagamento(data);
-      })
-      .catch(
-        ({
-          response: {
-            data: { mensagem },
-          },
-        }) => toast.error(mensagem)
-      )
-      .finally(() => setIsLoad(false));
+    // Pagamento.gerarFatura({ uuidAgendamento: agendamento?.uuid })
+    //   .then(({ data }) => {
+    //     setPagamento(data);
+    //   })
+    //   .catch(
+    //     ({
+    //       response: {
+    //         data: { mensagem },
+    //       },
+    //     }) => toast.error(mensagem)
+    //   )
+    //   .finally(() => setIsLoad(false));
   }, [agendamento]);
 
   return (
@@ -61,7 +61,7 @@ export const PaymentTemplate = () => {
             <PaymentsOptionsContainer
               name="pagamento"
               required
-              value={maskMoney((pagamento?.items_total_cents/100) + taxaPix)}
+              value={maskMoney(pagamento?.items_total_cents / 100 + taxaPix)}
               handleSelect={(e) => setPaymento(e)}
               iconLeft="/assets/imgs/pix-icon.svg"
               textIcon="PIX"
@@ -74,7 +74,7 @@ export const PaymentTemplate = () => {
               required
               handleSelect={(e) => setPaymento(e)}
               name="pagamento"
-              value={maskMoney((pagamento?.items_total_cents/100) + taxaBoleto)}
+              value={maskMoney(pagamento?.items_total_cents / 100 + taxaBoleto)}
               iconLeft="/assets/imgs/cod-barra.svg"
               textIcon="BOLETO"
             />
