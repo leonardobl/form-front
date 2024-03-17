2f9232fe02c1ede2beecb762a64eec65943c591e
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2024-03-17 14:20:20 -0300

    ajuste logo tokyo

--- src/components/Atoms/Button/styles.ts
+++ src/components/Atoms/Button/styles.ts
@@ -1,27 +1,34 @@
-import styled from "styled-components";
+import { lighten } from "polished";
+import styled, { css } from "styled-components";
 
 export const Button = styled.button`
-  width: 128px;
-  height: 44px;
+  ${({ theme: { colors } }) => css`
+    width: 128px;
+    height: 44px;
 
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(180deg, #41efc9 0%, #12d1a7 100%);
-  box-shadow: 0px 4px 4px 0px #00000033;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(
+      180deg,
+      ${colors.main} 0%,
+      ${lighten(0.1, colors.main)} 100%
+    );
+    box-shadow: 0px 4px 4px 0px #00000033;
 
-  font-family: Mulish;
-  font-size: 16px;
-  font-weight: 700;
-  white-space: nowrap;
-  line-height: 24px;
-  letter-spacing: 0em;
-  text-align: center;
-  color: #fff;
+    font-family: Mulish;
+    font-size: 16px;
+    font-weight: 700;
+    white-space: nowrap;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #fff;
 
-  &:disabled {
-    cursor: not-allowed;
-    background: #b8b8b8;
-  }
+    &:disabled {
+      cursor: not-allowed;
+      background: #b8b8b8;
+    }
+  `}
 `;
