1650966aa5c1adcf37189bb7dc5cd923796b9175
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2024-03-17 13:51:19 -0300

    ajuste cores da barra de menu

--- src/components/Atoms/Bar/styles.ts
+++ src/components/Atoms/Bar/styles.ts
@@ -1,11 +1,14 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const MyBar = styled.div`
-  width: 100%;
-  background: #12d1a7;
-  box-shadow: 0px 8px 8px 0px rgba(0, 0, 0, 0.16);
-  height: 90px;
-  padding: 0 42px;
-  display: flex;
-  align-items: center;
+  ${({ theme: { colors } }) => css`
+    width: 100%;
+    /* background: #12d1a7; */
+    background: ${colors.main};
+    box-shadow: 0px 8px 8px 0px rgba(0, 0, 0, 0.16);
+    height: 90px;
+    padding: 0 42px;
+    display: flex;
+    align-items: center;
+  `}
 `;
