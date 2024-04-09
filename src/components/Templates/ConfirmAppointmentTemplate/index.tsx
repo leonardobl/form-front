3ac59e9bcf3576762b9b6e82f36459e9cfccd2a6
Leonardo Lima <leonardo@paguextech.com>
2024-04-09 09:13:55 -0300

    exibicao da confirmacao do pagamento

--- src/components/Templates/ConfirmAppointmentTemplate/index.tsx
+++ src/components/Templates/ConfirmAppointmentTemplate/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import * as S from "./styles";
 import { Text } from "../../Atoms/Text";
 import { Input } from "../../Atoms/Inputs/Input";
+import { useConfirmAppointment } from "./useConfirmAppointment";
+import { maskCnpj, maskCpf, maskMoney } from "../../../utils/masks";
 
 export const ConfirmAppointmentTemplate = () => {
+  const { agendamento } = useConfirmAppointment();
+
   return (
     <S.Container>
       <S.WrapperText>
@@ -15,43 +19,87 @@ export const ConfirmAppointmentTemplate = () => {
       </S.WrapperText>
       <S.Form>
         <div>
-          <Input readOnly label="Tipo de Agendamento" />
+          <Input
+            value={agendamento?.veiculo?.tipo || " --- "}
+            readOnly
+            label="Tipo de Agendamento"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Cliente" />
+          <Input
+            value={agendamento?.cliente?.nome || " --- "}
+            readOnly
+            label="Cliente"
+          />
         </div>
 
         <div>
-          <Input readOnly label="CPF/CNPJ" />
+          <Input
+            value={
+              agendamento?.cliente?.cpfCnpj?.length > 14
+                ? maskCnpj(agendamento?.cliente?.cpfCnpj)
+                : maskCpf(agendamento?.cliente?.cpfCnpj)
+            }
+            readOnly
+            label="CPF/CNPJ"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Telefone" />
+          <Input
+            value={agendamento?.cliente?.telefone || " --- "}
+            readOnly
+            label="Telefone"
+          />
         </div>
 
         <div>
-          <Input readOnly label="E-mail" />
+          <Input
+            value={agendamento?.cliente?.email || " --- "}
+            readOnly
+            label="E-mail"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Modelo do carro" />
+          <Input
+            value={agendamento?.veiculo?.modelo || " --- "}
+            readOnly
+            label="Modelo do carro"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Placa" />
+          <Input
+            value={agendamento?.veiculo?.placa || " --- "}
+            readOnly
+            label="Placa"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Renavam" />
+          <Input
+            value={agendamento?.veiculo?.renavam || " --- "}
+            readOnly
+            label="Renavam"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Chassi" />
+          <Input
+            value={agendamento?.veiculo?.chassi || " --- "}
+            readOnly
+            label="Chassi"
+          />
         </div>
 
         <div>
-          <Input readOnly label="Valor Pago" />
+          <Input
+            value={maskMoney(agendamento?.servico?.valorPadrao)}
+            readOnly
+            label="Valor Pago"
+          />
         </div>
 
         <div>
