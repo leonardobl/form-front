e8a437afc1415cee123ab44cef9fc40d0a9216e2
Leonardo Lima <leonardo@paguextech.com>
2024-04-10 15:52:28 -0300

    criacao service colaborador listar-agendamentos

--- src/services/Colaborador/index.ts
+++ src/services/Colaborador/index.ts
@@ -1,11 +1,27 @@
 import { AxiosResponse } from "axios";
 import { ApiBrave } from "../../Apis/Brave";
 import objectToParams from "../../utils/objectToParams";
+import {
+  IColaboradorDTO,
+  IColaboradorForm,
+  IListarAgendamentosProps,
+} from "../../types/colaborador";
+import { IAgendamentoDTO } from "../../types/agendamento";
 
 const basePath = "/colaborador";
 
 export class Colaborador {
-  static async get(): Promise<AxiosResponse<any>> {
-    return ApiBrave.get(`${basePath}`);
+  static async get(): Promise<AxiosResponse<IColaboradorDTO[]>> {
+    return ApiBrave.get(basePath);
+  }
+
+  static async listarAgendamentos(
+    props: IListarAgendamentosProps
+  ): Promise<AxiosResponse<IAgendamentoDTO[]>> {
+    return ApiBrave.get(`${basePath}/listar-agendamentos`);
+  }
+
+  static async put(): Promise<AxiosResponse<IColaboradorForm>> {
+    return ApiBrave.put(`${basePath}/cadastrar`);
   }
 }
--- src/types/colaborador.d.ts
+++ src/types/colaborador.d.ts
@@ -10,3 +10,19 @@ export interface IColaboradorDTO {
   uuid: string;
   uuidUsuario: string;
 }
+
+export interface IColaboradorForm {
+  cpf: string;
+  dataNascimento?: string;
+  nome: string;
+  sexo?: SexoEnum;
+  tipo: TipoColaboradorEnum;
+  uuidDelivery?: string;
+  uuidLoja?: string;
+  uuidUsuario?: string;
+}
+
+export interface IListarAgendamentosProps {
+  uuidColaborador: string;
+  data: string;
+}
