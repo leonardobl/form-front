84c220e31b210b2194a33876859fc7969effe62a
Leonardo Lima <leonardo@paguextech.com>
2024-04-18 17:09:39 -0300

    criacao do filtro

--- src/components/Templates/SettingsTemplate/index.tsx
+++ src/components/Templates/SettingsTemplate/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import * as S from "./styles";
 import { LayoutTemplate } from "../LayoutTemplate";
 import { Title } from "../../Atoms/Title";
+import { Input } from "../../Atoms/Inputs/Input";
+import { SimpleSelect } from "../../Atoms/Selects/SimpleSelect";
+import { Button } from "../../Atoms/Button";
 
 export const SettingsTemplate = () => {
   return (
@@ -10,6 +13,26 @@ export const SettingsTemplate = () => {
         <Title>Concessionárias Cadastradas</Title>
         <S.Filter>
           <S.FilterTitle>Filtro</S.FilterTitle>
+          <S.FilterContent>
+            <div>
+              <Input label="Nome" />
+            </div>
+            <div>
+              <Input label="CNPJ" />
+            </div>
+
+            <div>
+              <SimpleSelect />
+            </div>
+
+            <div>
+              <button>Limpar tudo</button>
+            </div>
+
+            <div>
+              <Button>BUSCAR</Button>
+            </div>
+          </S.FilterContent>
         </S.Filter>
       </S.Container>
     </LayoutTemplate>
--- src/components/Templates/SettingsTemplate/styles.ts
+++ src/components/Templates/SettingsTemplate/styles.ts
@@ -12,5 +12,46 @@ export const Container = styled.div`
   }
 `;
 
-export const Filter = styled.form``;
-export const FilterTitle = styled.h2``;
+export const Filter = styled.form`
+  overflow: hidden;
+  border-radius: 10px 10px 0px 0px;
+  border: 1px solid #12d1a7;
+`;
+
+export const FilterTitle = styled.h2`
+  background: #12d1a7;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  padding: 20px 0;
+  color: #fff;
+  font-family: Mulish;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 32px; /* 133.333% */
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+export const FilterContent = styled.div`
+  padding: 40px 90px 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-areas: "nome nome nome nome" "cnpj cidade limpar buscar";
+  gap: 32px 16px;
+
+  > :nth-child(1) {
+    grid-area: nome;
+  }
+  > :nth-child(2) {
+    grid-area: cnpj;
+  }
+  > :nth-child(3) {
+    grid-area: cidade;
+  }
+  > :nth-child(4) {
+    grid-area: limpar;
+  }
+  > :nth-child(5) {
+    grid-area: buscar;
+  }
+`;
