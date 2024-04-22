0341536edfa3cd94dd48cb5e286894d57b52f4b3
Leonardo Lima <leonardo@paguextech.com>
2024-04-22 13:20:03 -0300

    criacao da useSetting

--- src/components/Templates/SettingsTemplate/useSettings.ts
+++ src/components/Templates/SettingsTemplate/useSettings.ts
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useSettings = () => {
   const navigate = useNavigate();
+
+  const getConcessionarias = useCallback(() => {}, []);
+
+  useEffect(() => {
+    getConcessionarias();
+  }, []);
 
   return { navigate };
 };
