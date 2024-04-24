17018c183e0d9f74544bd4dcc8d4109cd6bad99c
Leonardo Lima <leonardo@paguextech.com>
2024-04-24 13:32:36 -0300

    separacao das rotas

---
+++ src/routes/useMainRoutes.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Navigate, Route } from "react-router-dom";
+import { ProtectedRoute } from "../components/Atoms/ProtectedRoute";
+import { ProtectedAdminRoute } from "../components/Atoms/ProtectedAdminRoute";
+import { NewScheduling } from "../components/Pages/NewScheduling";
+import { Profile } from "../components/Pages/Profile";
+import { Offline } from "../components/Pages/Offline";
+import { ResetPassword } from "../components/Pages/ResetPassword";
+import { DocDownloads } from "../components/Atoms/DocDownload";
+import { NotFound } from "../components/Atoms/NotFound";
+
+export const useMainRoutes = () => {
+  return (
+    <>
+      <Route index element={<Navigate to={"/agendamento"} />} />
+      <Route path="/download-comprovante/:id" element={<DocDownloads />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/alterar-senha" element={<ResetPassword />} />
+      <Route path="/offline" element={<Offline />} />
+      <Route
+        path="/perfil"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/novo-agendamento"
+        element={
+          <ProtectedRoute>
+            <ProtectedAdminRoute>
+              <NewScheduling />
+            </ProtectedAdminRoute>
+          </ProtectedRoute>
+        }
+      />
+    </>
+  );
+};
