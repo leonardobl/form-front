f473d986a33b621fc9ba49b835e0f5ed671524e9
Leonardo Lima <leonardo.ti@braveconsultoria.com>
2024-04-26 17:51:56 -0300

    remocao de comentarios

--- src/components/Templates/StoreTemplate/useStore.ts
+++ src/components/Templates/StoreTemplate/useStore.ts
@@ -1,52 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { useSearchParams } from "react-router-dom";
 
 export const useStore = () => {
   const [searchParams] = useSearchParams();
   const reagendamento = !!(searchParams.get("reagendamento") === "true");
-  // useEffect(() => {
-  //   setReagendamentoForm((prev) => ({ ...prev, horaAgendada: null }));
-  //   if (date) {
-  //     const newDate = date.toLocaleDateString().split("/").reverse().join("-");
-
-  //     if (reagendamentoForm?.uuidLoja) {
-  //       Loja.getHorariosDisponiveis({
-  //         uuidLoja: reagendamentoForm?.uuidLoja,
-  //         dataAgendamento: newDate,
-  //       }).then(({ data }) => {
-  //         const options = data.map((item) => ({
-  //           value: item,
-  //           label: item,
-  //           element: item,
-  //         }));
-
-  //         setHorariosOptions(options);
-  //       });
-  //     }
-  //   }
-  // }, [date]);
-
-  // useEffect(() => {
-  //   setDate(null);
-  //   if (reagendamentoForm?.uuidLoja) {
-  //     setIsLoading(true);
-  //     Loja.getDiasIndisponiveis({ uuidLoja: reagendamentoForm.uuidLoja })
-  //       .then(({ data }) => {
-  //         const options = data.map((item) => addDays(new Date(item), 1));
-
-  //         setDiasIndisponiveis(options);
-  //       })
-  //       .catch(
-  //         ({
-  //           response: {
-  //             data: { mensagem },
-  //           },
-  //         }) => toast.error(mensagem)
-  //       )
-  //       .finally(() => setIsLoading(false));
-  //   }
-  // }, [reagendamentoForm?.uuidLoja]);
 
   return {
     reagendamento,
