105e6d4ee0b0165e07586dc68cf015874a02bf66
Leonardo Lima <leonardo@paguextech.com>
2024-06-07 11:12:33 -0300

    criacao do aceite de vistoria

--- src/components/Templates/AcceptInspectionTemplate/index.tsx
+++ src/components/Templates/AcceptInspectionTemplate/index.tsx
@@ -3,60 +3,97 @@ import * as S from "./styles";
 import { Input } from "../../Atoms/Inputs/Input";
 import { Button } from "../../Atoms/Button";
 import { useAcceptInspection } from "./useAcceptInspection";
+import { maskCep, maskPhone } from "../../../utils/masks";
 
 export const AcceptInspectionTemplate = () => {
-  const {} = useAcceptInspection();
+  const { atribuirAgendamento, vistoria } = useAcceptInspection();
 
   return (
     <S.Container>
       <S.Form>
         <div>
-          <Input readOnly label="Nome" />
+          <Input readOnly label="Nome" value={vistoria?.cliente?.nome} />
         </div>
         <div>
-          <Input readOnly label="Telefone" />
+          <Input
+            readOnly
+            label="Telefone"
+            value={maskPhone(vistoria?.cliente?.telefone)}
+          />
         </div>
         <div>
-          <Input readOnly label="CEP" />
+          <Input
+            readOnly
+            label="CEP"
+            value={maskCep(vistoria?.cliente?.endereco?.cep)}
+          />
         </div>
         <div>
-          <Input readOnly label="Endereço (Rua)" />
+          <Input
+            readOnly
+            label="Endereço (Rua)"
+            value={vistoria?.cliente?.endereco?.logradouro}
+          />
         </div>
         <div>
-          <Input readOnly label="Número" />
+          <Input
+            readOnly
+            label="Número"
+            value={vistoria?.cliente?.endereco?.numero}
+          />
         </div>
         <div>
-          <Input readOnly label="Complemento" />
+          <Input
+            readOnly
+            label="Complemento"
+            value={vistoria?.cliente?.endereco?.complemento}
+          />
         </div>
         <div>
-          <Input readOnly label="Bairro" />
+          <Input
+            readOnly
+            label="Bairro"
+            value={vistoria?.cliente?.endereco?.bairro}
+          />
         </div>
         <div>
-          <Input readOnly label="UF" />
+          <Input readOnly label="UF" value={vistoria?.cliente?.endereco?.uf} />
         </div>
         <div>
-          <Input readOnly label="Cidade" />
+          <Input
+            readOnly
+            label="Cidade"
+            value={vistoria?.cliente?.endereco?.cidade}
+          />
         </div>
         <div>
-          <Input readOnly label="Modelo do Carro" />
+          <Input
+            readOnly
+            label="Modelo do Carro"
+            value={vistoria?.veiculo?.modelo}
+          />
         </div>
         <div>
-          <Input readOnly label="Tipo de Veículo" />
+          <Input
+            readOnly
+            label="Tipo de Veículo"
+            value={vistoria?.veiculo?.tipo}
+          />
         </div>
         <div>
-          <Input readOnly label="Ano" />
+          <Input readOnly label="Ano" value={vistoria?.veiculo?.ano} />
         </div>
         <div>
-          <Input readOnly label="Placa" />
+          <Input readOnly label="Placa" value={vistoria?.veiculo?.placa} />
         </div>
         <div>
-          <Input readOnly label="Renavam" />
+          <Input readOnly label="Renavam" value={vistoria?.veiculo?.renavam} />
         </div>
         <div>
-          <Input readOnly label="Chassi" />
+          <Input readOnly label="Chassi" value={vistoria?.veiculo?.chassi} />
         </div>
         <div>
-          <Button>Aceite</Button>
+          <Button onClick={atribuirAgendamento}>Aceite</Button>
         </div>
       </S.Form>
     </S.Container>
--- src/components/Templates/AcceptInspectionTemplate/useAcceptInspection.ts
+++ src/components/Templates/AcceptInspectionTemplate/useAcceptInspection.ts
@@ -1,9 +1,92 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import {
+  IAgendamentoDTO,
+  IIniciarAgendamentoProps,
+} from "../../../types/agendamento";
+import { Agendamento } from "../../../services/Agendamento";
+import { useContextSite } from "../../../context/Context";
+import { toast } from "react-toastify";
+import { IColaboradorCompletoDTO } from "../../../types/colaborador";
+import { Colaborador } from "../../../services/Colaborador";
 
 export const useAcceptInspection = () => {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
+  const [vistoria, setVistoria] = useState<IAgendamentoDTO>(
+    {} as IAgendamentoDTO
+  );
+  const { setIsLoad } = useContextSite();
 
-  return {};
+  const [colaboradorAtual, setColaboradorAtual] =
+    useState<IColaboradorCompletoDTO>({} as IColaboradorCompletoDTO);
+
+  const getColaboradorAtual = useCallback(() => {
+    Colaborador.atual()
+      .then(({ data }) => {
+        setColaboradorAtual(data);
+      })
+      .catch(
+        ({
+          response: {
+            data: { mensagem },
+          },
+        }) => {
+          toast.error(mensagem);
+        }
+      );
+  }, []);
+
+  useEffect(() => {
+    getColaboradorAtual();
+  }, [getColaboradorAtual]);
+
+  useEffect(() => {
+    if (id) {
+      setIsLoad(true);
+      Agendamento.getById({ uuid: id })
+        .then(({ data }) => setVistoria(data))
+        .catch(
+          ({
+            response: {
+              data: { mensagem },
+            },
+          }) => toast.error(mensagem)
+        )
+        .finally(() => {
+          setIsLoad(false);
+        });
+    }
+  }, [id]);
+
+  function atribuirAgendamento(e: React.SyntheticEvent) {
+    e.preventDefault();
+
+    const PAYLOAD: IIniciarAgendamentoProps = {
+      uuid: vistoria?.uuid,
+      uuidVistoriador: colaboradorAtual.uuid,
+      uuidAtendente: colaboradorAtual?.uuid,
+    };
+
+    setIsLoad(true);
+
+    Agendamento.atribuir(PAYLOAD)
+      .then(() => {
+        toast.success("Aceite realizado com sucesso!");
+      })
+      .catch(
+        ({
+          response: {
+            data: { mensagem },
+          },
+        }) => {
+          toast.error(mensagem);
+        }
+      )
+      .finally(() => {
+        setIsLoad(false);
+      });
+  }
+
+  return { vistoria, atribuirAgendamento };
 };
