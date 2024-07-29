c9c18d851df5bde7040eb34a9a6731b31b67bacf
Leonardo Lima <leonardo@paguextech.com>
2024-07-29 10:35:01 -0300

    criacao do input checkslide

---
+++ src/components/Templates/ItinerantRegisterTemplate/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "../../Atoms/Container";
+import { InputCheckSlide } from "../../Atoms/Inputs/InputCheckSlide";
+
+export const ItinerantRegisterTemplate = () => {
+  return (
+    <Container>
+      <InputCheckSlide label="Horário de Almoço" />
+    </Container>
+  );
+};
